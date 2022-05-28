b01589e97cc48498ae31d439ae26307d015bada6
MikeMerzliakov <2281205@gmail.com>
2022-05-29 00:04:08 +0300

    add PromiseRequest JS

---
+++ HW22 GetUsers/PromiseRequest.js
@@ -0,0 +1,42 @@
+let children=[];
+
+const getFile = (file , i) =>{ 
+    let xhr = new XMLHttpRequest();
+    xhr.open (`GET`, `${file}`);
+    xhr.send();
+    xhr.addEventListener (`readystatechange`, () => {
+        if(xhr.readyState === 4){
+            if (xhr.status<399 && xhr.status >= 200){
+                let Obj = JSON.parse(xhr.response);
+                children.push(...Obj.children);
+                
+                i && console.log(children);
+            }
+            else{
+                console.log(`file: ${file} || error TEXT: ${xhr.statusText}`);
+            }
+        }
+    })
+}
+
+let myPromise = new Promise(
+    function(resolve){
+        resolve()
+    }
+)
+myPromise
+    .then(
+        function(){ getFile(`./DB-files/data.json`) }
+    )
+    .then(
+        function(){ getFile(`./DB-files/data2.json`,true)  }
+    )
+    .then(
+        function(){ 
+            console.log(`Job is done! 💚`); 
+            console.log(children); 
+        }
+    )
+   
+//http://djotto.dyndns.org/work.fe/data.json
+//http://djotto.dyndns.org/work.fe/data2.json
