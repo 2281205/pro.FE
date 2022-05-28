8215bce76016638f1b40ffe936e13a27f3496348
MikeMerzliakov <2281205@gmail.com>
2022-05-28 21:29:04 +0300

    for 23. XHR request

---
+++ HW22 GetUsers/XHRrequest.js
@@ -0,0 +1,25 @@
+let children=[]
+
+const getFile = (file, cb, i) =>{ 
+    let xhr = new XMLHttpRequest();
+    xhr.open (`GET`, `${file}`);
+    xhr.send();
+    xhr.addEventListener (`readystatechange`, () => {
+        if(xhr.readyState === 4 && xhr.status<399 && xhr.status >= 200 )
+        {
+        let Obj = JSON.parse(xhr.response);
+        return cb(Obj.children, i);
+        }
+    })
+}
+
+const renderFile = (file, i) => {
+    children.push(...file);
+    i === 2 && console.log(children);
+}
+
+getFile(`./DB-files/data.json`, renderFile, 1)
+getFile(`./DB-files/data2.json`, renderFile, 2)
+
+//http://djotto.dyndns.org/work.fe/data.json
+//http://djotto.dyndns.org/work.fe/data2.json
--- HW22 GetUsers/request.js
+++
@@ -1,4 +0,0 @@
-console.log(`work! request.js`);
-
-
-
